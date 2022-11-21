1107b641654bf4907cc40432a65480b086a7367a
davonbl-bl <davonbl@gmail.com>
2022-11-21 14:40:56 -0500

    create a folder structure for the server side, install required dependencies, and add the basic routes for the API

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(express.json())
+app.use(express.static('public'));
+
+const inventoryRoutes = require('./routes/inventory');
+const wareHouseRoutes =  require('./routes/wareHouse');
+
+app.use('/inventory', inventoryRoutes);
+app.use ('/wareHouse', wareHouseRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Testing"); 
+})
+
+app.listen(PORT, () => {
+    console.log('Server stated on http://localhost:8080');
+    console.log("Press CTRL + C to stop server"); 
+})
---
+++ routes/inventory.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const fs = require('fs')
+const router = express.Router()
+
+
+
+module.exports = router;
