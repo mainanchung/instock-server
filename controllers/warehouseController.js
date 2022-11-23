2d80fcb39270a6453cdd014b191fdef636cc7f02
Tlau425 <tlau425@yahoo.com>
2022-11-23 14:12:53 -0500

    editing warehouse backend

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -2,33 +2,44 @@ const knex = require('knex')(require('../knexfile'))
 
 exports.index = (req, res) => {
     knex('warehouses')
-    .then((data) => {
-        res.status(200).json(data);
-    })
-    .catch((err) => 
-        res.status(400).send(`Error retrieving Warehouses: ${err}`)
-    )
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((err) =>
+            res.status(400).send(`Error retrieving Warehouses: ${err}`)
+        )
 }
 
 exports.warehouseById = (req, res) => {
     knex('warehouses')
-    .where('warehouses.id', '=', req.params.id)
-    .join('inventories', 'inventories.warehouse_id', '=', 'warehouses.id')
-    .select('*', 'warehouses.id as id', 'inventories.id as inventory_id', )
-    .then((data) => {
-        if (!data.length) {
-            knex('warehouses').where('warehouses.id', '=', req.params.id)
-            .then((data) => {
-            
-                console.log(data)
-               res.json(data)
-            })
-        } else {
-            res.status(200).json(data);
-        }
-        
-    })
-    .catch((err) => 
-        res.status(400).send(`Error retrieving warehouses: ${err}`)
-    )
+        .where('warehouses.id', '=', req.params.id)
+        .join('inventories', 'inventories.warehouse_id', '=', 'warehouses.id')
+        .select('*', 'warehouses.id as id', 'inventories.id as inventory_id',)
+        .then((data) => {
+            if (!data.length) {
+                knex('warehouses').where('warehouses.id', '=', req.params.id)
+                    .then((data) => {
+                        console.log(data)
+                        res.status(200).json(data)
+                    })
+            } else {
+                res.status(200).json(data);
+            }
+
+        })
+        .catch((err) =>
+            res.status(400).send(`Error retrieving warehouses: ${err}`)
+        )
+}
+
+exports.warehouseUpdate = (req, res) => {
+    knex("warehouses")
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) =>{
+            res.status(400).send(`Invalid field input: ${error}`)
+        })
 }
--- routes/wareHouse.js
+++ routes/wareHouse.js
@@ -5,6 +5,8 @@ const warehouseController = require('../controllers/warehouseController');
 
 router.route('/').get(warehouseController.index);
 
-router.route('/:id').get(warehouseController.warehouseById);
+router.route('/:id')
+    .get(warehouseController.warehouseById)
+    .patch(warehouseController.warehouseUpdate);
 
 module.exports = router;
