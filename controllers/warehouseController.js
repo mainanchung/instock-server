c38f8d5a18a430dd374bd33e5bf98af515e8a69f
Tlau425 <tlau425@yahoo.com>
2022-11-23 12:24:32 -0500

    warehouse-id-change

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,7 +1,6 @@
 const knex = require('knex')(require('../knexfile'))
 
 exports.index = (req, res) => {
-
     knex('warehouses')
     .then((data) => {
         res.status(200).json(data);
@@ -17,9 +16,19 @@ exports.warehouseById = (req, res) => {
     .join('inventories', 'inventories.warehouse_id', '=', 'warehouses.id')
     .select('*', 'warehouses.id as id', 'inventories.id as inventory_id', )
     .then((data) => {
-        res.status(200).json(data);
+        if (!data.length) {
+            knex('warehouses').where('warehouses.id', '=', req.params.id)
+            .then((data) => {
+            
+                console.log(data)
+               res.json(data)
+            })
+        } else {
+            res.status(200).json(data);
+        }
+        
     })
     .catch((err) => 
-    res.status(400).send(`Error retrieving warehouses: ${err}`)
+        res.status(400).send(`Error retrieving warehouses: ${err}`)
     )
 }
